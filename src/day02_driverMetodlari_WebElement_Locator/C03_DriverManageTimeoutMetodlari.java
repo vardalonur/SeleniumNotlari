476931d8498f4ec51b14101b53558dc145404af0
vardal <vardalonur@gmail.com>
2024-07-10 14:09:59 +0300

    day02

--- src/day02_driverMetodlari_WebElement_Locator/C03_DriverManageTimeoutMetodlari.java
+++ src/day02_driverMetodlari_WebElement_Locator/C03_DriverManageTimeoutMetodlari.java
@@ -1,2 +1,47 @@
-package day02_driverMetodlari_WebElement_Locator;public class C03_DriverManageTimeoutMetodlari {
+package day02_driverMetodlari_WebElement_Locator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_DriverManageTimeoutMetodlari {
+    public static void main(String[] args) {
+        System.setProperty("Webdriver.chrome.driver","KurulumDosyalari/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        /*
+        Thread.sleep() java'dan gelir
+        ve kodların çalışmasını bizim belirlediğimiz müddetçe durdurur.
+
+        Testi ilk defa yazarken test adımlarını daha rahat görebilmek
+        veya bir sunum yaptığımızda sunum yaptığımız kişilere adımları
+        gösterebilmemiz açısından kullanımı uygundur.
+
+        Bazen de bilgisyarımızın veya internetin hızından dolayı kodlar
+        hızlı ilerlediğinde istediğimiz adımları gerçekleştiremeyebiliriz
+        bu durumda mecbur kalırsak Thread.sleep kullanabiliriz.
+
+        Eğer testi hazırlamayı bitirdik ve hız açısından da beklemeyi mecbur
+        kılan bir durum yoksa Thread.sleep'leri silmekte fayda vardır.
+         */
+
+        /*
+        implicitlyWait farklı olarak göreve dayalı beklemeyi yapar.
+        biz 10 saniye tanımladığımızda görevi yapmak için max 10sn
+        bekler, daha erken biterse yoluna devam eder. 10sn'nin dolmasını
+        beklemez.
+        Ancak, 10sn dolmasına rağmen görev bitmediyse exception fırlatır
+        ve çalışmayı durdurur.
+
+        implicitlyWait iki temel görev için bekleme yapar
+        1- Sayfanın açılıp ögelerin yüklenmesi
+        2- Herhangi bir web ögesini kullanmamız gerektiğinde o ögeyi bulmak
+
+         */
+
+        driver.quit();
+    }
 }
--- src/day02_driverMetodlari_WebElement_Locator/C05_LocatorVeWebElementKullanimi.java
+++ src/day02_driverMetodlari_WebElement_Locator/C05_LocatorVeWebElementKullanimi.java
@@ -1,2 +1,47 @@
-package day02_driverMetodlari_WebElement_Locator;public class C05_LocatorVeWebElementKullanimi {
+package day02_driverMetodlari_WebElement_Locator;
+
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C05_LocatorVeWebElementKullanimi {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","KurulumDosyalari/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //testotomasynu.com anasayfaya gidin
+        driver.get("https://testotomasyonu.com");
+
+
+        //arama kutusunu locate edin
+            /*id'si global-search olan webelement dediğimizde unique
+            değere ulaşıyoruz
+            locator olarak By.id() kullanabiliriz.
+            locate etme, belirlenen unique adresi ve locator'ı kullanarak
+            istenen web elementi içinde bulunduğumuz class'dan kullanılır
+            hale getirmektir.
+             */
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+
+
+        //arama kutusuna "phone" yazdırın
+        aramaKutusu.sendKeys("phone");
+
+        //enter'a basarak aramayı yaptırın
+
+        aramaKutusu.submit();
+
+        //sayfayı kapatın
+
+        driver.quit();
+
+    }
 }
